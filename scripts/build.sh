5958e342d6e09461532c3bc9e0adb91c19bafd11
Vincent Emonet <vincent.emonet@gmail.com>
2023-09-20 16:22:21 +0200

    upgrade widoco version

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,15 +7,14 @@ rm -rf docs/
 # Generate docs with widoco
 java -jar widoco.jar -ontFile $ONTOLOGY_FILE -outFolder docs -getOntologyMetadata -oops -rewriteAll -webVowl -noPlaceHolderText
 mv docs/index-en.html docs/index.html
-mv docs/OOPSevaluation docs/oops
-mv docs/oops/oopsEval.html docs/oops/index.html
 
 # Generate JSON-LD context
-java -jar owl2jsonld.jar http://pubannotation.org/ontology/tao.owl > docs/context.jsonld
+java -jar owl2jsonld.jar https://raw.githubusercontent.com/vemonet/tao/main/tao.owl > docs/context.jsonld
+
 
 # Generate docs with Ontospy
 mkdir -p docs/browse
-ontospy gendocs -o docs/browse --type 2 --preflabel label --nobrowser docs/ontology.rdf
+ontospy gendocs -o docs/browse --type 2 --preflabel label --nobrowser docs/ontology.owl
 
 # Alternative ontospy visualizations (e.g. tree and graph)
 # ontospy gendocs -o docs/tree --type 4 --preflabel label --nobrowser $ONTOLOGY_FILE
@@ -24,10 +23,6 @@ ontospy gendocs -o docs/browse --type 2 --preflabel label --nobrowser docs/ontol
 
 # Add "Browse with Ontospy" button
 find docs/index.html -type f -exec sed -i "s/alt=\"Visualize with WebVowl\" \/><\/a><\/dd>/alt=\"Visualize with WebVowl\" \/><\/a>\n<a href=\"browse\" target=\"_blank\"><img src=\"https:\/\/img.shields.io\/badge\/Browse_with-Ontospy-orange.svg\" alt=\"Browse with Ontospy\" \/><\/a><\/dd>/g" {} +
-
-# Add "Evaluate with OOPS" button
-find docs/index.html -type f -exec sed -i "s/<!-- <dt>Evaluation:<\/dt><dd><a href=\"OOPSEvaluation\/oopsEval.html#/<dt>Evaluation:<\/dt><dd><a href=\"oops/g" {} +
-find docs/index.html -type f -exec sed -i "s/<\/dd> -->/<\/dd>/g" {} +
 
 # Add favicon
 find docs/index.html -type f -exec sed -i "s/<head>/<head>\n<link rel=\"icon\" type=\"image\/x-icon\" href=\"https:\/\/pubannotation.org\/favicon.ico\">/g" {} +
--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@
 pip install ontospy
 
 # https://github.com/dgarijo/Widoco
-wget -O widoco.jar https://github.com/dgarijo/Widoco/releases/download/v1.4.17/java-17-widoco-1.4.17-jar-with-dependencies.jar
+wget -O widoco.jar https://github.com/dgarijo/Widoco/releases/download/v1.4.20/widoco-1.4.20-jar-with-dependencies_JDK-17.jar
 
 # https://github.com/stain/owl2jsonld
 wget -O owl2jsonld.jar https://github.com/stain/owl2jsonld/releases/download/0.2.1/owl2jsonld-0.2.1-standalone.jar
